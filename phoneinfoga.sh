294deb83d9e364b74fae14cf277011ccbb81a2e2
ExpertAnonymous <60571839+ExpertAnonymous@users.noreply.github.com>
2020-06-02 13:28:58 +0545

    Update phoneinfoga.sh

--- phoneinfoga.sh
+++ phoneinfoga.sh
@@ -42,7 +42,7 @@ rm -rf phoneinfoga > /dev/null 2>&1
 echo
 sleep 2.0
 echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m PhoneInfoga Installing...'
-git clone https://github.com/ExpertAnonymousOfficial/PhoneInfoga > /dev/null 2>&1 && cd PhoneInfoga > /dev/null 2>&1 && unzip PhoneInfoga > /dev/null 2>&1 && python3 -m pip install -r requirements.txt > /dev/null 2>&1 && pip2 install colorama > /dev/null 2>&1
+git clone https://github.com/ExpertAnonymous/PhoneInfoga > /dev/null 2>&1 && cd PhoneInfoga > /dev/null 2>&1 && unzip PhoneInfoga > /dev/null 2>&1 && python3 -m pip install -r requirements.txt > /dev/null 2>&1 && pip2 install colorama > /dev/null 2>&1
 rm -rf PhoneInfoga.zip > /dev/null 2>&1
 rm -rf phoneinfoga.sh > /dev/null 2>&1
 chmod +x *
